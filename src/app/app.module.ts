0df135e90eb3326024385185f9e5013cd94feda6
petyusa <peter.vincze.dev@gmail.com>
2018-06-29 12:21:23 +0200

    added main components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [
+    AppComponent,
+    CurrentTrainingComponent,
+    LoginComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    SignupComponent,
+    TrainingComponent,
+    WelcomeComponent
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
