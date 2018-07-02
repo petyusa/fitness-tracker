077bd26d90d8422b02cbbd118052927bc97ff7f9
Peter Vincze <peter.vincze.dev@gmail.com>
2018-07-02 07:24:26 +0200

    impplemented current/new training components

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  timer: any;
+  @Output() trainingExit = new EventEmitter();
 
-  constructor() { }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
+    this.startOrResumeTimer();
   }
 
+  startOrResumeTimer() {
+    this.timer = setInterval(() => {
+      this.progress += 1;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 50);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
+  }
 }
